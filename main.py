e9325bb40ca324e384c05b37a0d0646390689062
James Hecking <jhecking_10@protonmail.com>
2026-08-27 21:55:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def greeting():
     print("========== LIFT TRACKER ==========")
-    return input("Push, pull, or leg day?\n")
+    while True:
+        routine = input("Input today's routine as either push, pull, or legs:\n")
+        if routine != "push" and routine != "pull" and routine != "legs":
+            print("Please type push, pull or legs.")
+        else:
+            break
+    return routine
 
 def initialize(routine):
     print(f"Initializing data for {routine} day...")
